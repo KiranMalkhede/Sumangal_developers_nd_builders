0d1f0e0a3ce0074e6ccff490d1d6759ac274f116
Kiran Malkhede <kirannmalkhede@gmail.com>
2022-09-08 13:00:55 +0530

    today-8th-sep

--- resources/views/menu/reports/customers_details.blade.php
+++ resources/views/menu/reports/customers_details.blade.php
@@ -8,111 +8,124 @@
             <div class="card-header">
                 <div class="row">
                     <div class="col-lg-12 text-center">
-                        <h3>Customer's Details</h3>
+                        <h4>Search Customer's Details</h4>
                     </div>
                 </div>
             </div>
-            <div class="card-body" style="background-color: rgb(168, 236, 236)">
-                <form action="">
-                    <div class="row g-3 ">
-                        <div class="col-auto">
-                            <label for="inputPassword6" class="col-form-label">Reg.No</label>
+            <div class="card-body">
+                <form action="/search/customer" method="get">
+                    @csrf
+                    <div class="row g-3 py-3">
+                        <div class="col-1"></div>
+                        <div class="col-2">
+                            <label for="inputtext6" class="col-form-label">Customer Name</label>
                         </div>
-                        <div class="col-auto">
-                            <input type="password" id="inputPassword6" class="form-control"
-                                aria-describedby="passwordHelpInline">
+                        <div class="col-5">
+                            <input type="search" id="inputtext6" name="search" class="form-control border-primary"
+                                autofocus>
                         </div>
-                        <div class="col-auto">
-                            <label for="inputPassword6" class="col-form-label">Reg.Date</label>
-                        </div>
-                        <div class="col-auto">
-                            <input type="password" id="inputPassword6" class="form-control"
-                                aria-describedby="passwordHelpInline">
-                        </div>
-                    </div>
-                    <div class="row g-3  mt-2">
-                        <div class="col-auto">
-                            <label for="inputPassword6" class="col-form-label">Purchaser's name</label>
-                        </div>
-                        <div class="col">
-                            <input type="password" id="inputPassword6" class="form-control"
-                                aria-describedby="passwordHelpInline">
-                        </div>
-                    </div>
-                    <div class="row g-3  mt-2">
-                        <div class="col-auto">
-                            <label for="inputPassword6" class="col-form-label">Address </label>
-                        </div>
-                        <div class="col">
-                            <input type="password" id="inputPassword6" class="form-control"
-                                aria-describedby="passwordHelpInline">
-                        </div>
-                    </div>
-                    <hr>
-                    <div class="row g-3  mt-2">
-                        <div class="col-auto">
-                            <label for="inputPassword6" class="col-form-label">Executive name</label>
-                        </div>
-                        <div class="col">
-                            <input type="password" id="inputPassword6" class="form-control"
-                                aria-describedby="passwordHelpInline">
-                        </div>
-                        <div class="col-auto">
-                            <label for="inputPassword6" class="col-form-label">Executive Code</label>
-                        </div>
-                        <div class="col-auto">
-                            <input type="password" id="inputPassword6" class="form-control"
-                                aria-describedby="passwordHelpInline">
-                        </div>
-                    </div>
-                    <hr>
-                    <div class="row g-3 ">
-                        <div class="col-auto">
-                            <label for="inputPassword6" class="col-form-label">Mouza</label>
-                        </div>
-                        <div class="col-auto">
-                            <input type="password" id="inputPassword6" class="form-control"
-                                aria-describedby="passwordHelpInline">
-                        </div>
-                        <div class="col-auto">
-                            <label for="inputPassword6" class="col-form-label">Plot no.</label>
-                        </div>
-                        <div class="col-auto">
-                            <input type="password" id="inputPassword6" class="form-control"
-                                aria-describedby="passwordHelpInline">
-                        </div>
-                        <div class="col-auto">
-                            <label for="inputPassword6" class="col-form-label">Khasara No</label>
-                        </div>
-                        <div class="col-auto">
-                            <input type="password" id="inputPassword6" class="form-control"
-                                aria-describedby="passwordHelpInline">
-                        </div>
-                    </div>
-                    <div class="row g-3  mt-1">
-                        <div class="col-auto">
-                            <label for="inputPassword6" class="col-form-label">Plot Cost</label>
-                        </div>
-                        <div class="col-auto">
-                            <input type="password" id="inputPassword6" class="form-control"
-                                aria-describedby="passwordHelpInline">
-                        </div>
-                        <div class="col-auto">
-                            <label for="inputPassword6" class="col-form-label">Size</label>
-                        </div>
-                        <div class="col-auto">
-                            <input type="password" id="inputPassword6" class="form-control"
-                                aria-describedby="passwordHelpInline">
-                        </div>
-                        <div class="col-auto">
-                            <label for="inputPassword6" class="col-form-label">Type</label>
-                        </div>
-                        <div class="col-auto">
-                            <input type="password" id="inputPassword6" class="form-control"
-                                aria-describedby="passwordHelpInline">
+                        <div class="col-3">
+                            <div class="d-grid gap-2 mx-auto">
+                                <button type="submit" class="btn btn-outline-primary btn-lg">Search</button>
+                            </div>
                         </div>
                     </div>
                 </form>
+                @foreach ($customerdetails as $customerdetail)
+                    <div class="row g-3 py-3">
+                        <div class="col-1">
+                            <label for="inputtext6" class="col-form-label">Reg.No</label>
+                        </div>
+                        <div class="col-3">
+                            <input type="text" id="inputtext6" class="form-control border-primary"
+                                value="{{ $customerdetail->srNo }}" readonly>
+                        </div>
+                        <div class="col-4"></div>
+                        <div class="col-1">
+                            <label for="inputtext6" class="col-form-label">Reg.Date</label>
+                        </div>
+                        <div class="col-3">
+                            <input type="text" id="inputtext6" class="form-control border-primary"
+                                value="{{ $customerdetail->date }}" readonly>
+                        </div>
+                        <div class="col-1">
+                            <label for="inputtext6" class="col-form-label">Purchaser's <br> name</label>
+                        </div>
+                        <div class="col-5">
+                            <input type="text" id="inputtext6" style="text-transform: capitalize"
+                                value="{{ $customerdetail->name2 }}" class="form-control border-primary" readonly>
+                        </div>
+                        <div class="col-1">
+                            <label for="inputtext6" class="col-form-label">Address </label>
+                        </div>
+                        <div class="col-5">
+                            <textarea type="text" id="inputtext6" class="form-control border-primary" readonly>{{ $customerdetail->add }}</textarea>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row g-3  mt-2">
+                        <div class="col-1">
+                            <label for="inputtext6" class="col-form-label">Executive <br> name</label>
+                        </div>
+                        <div class="col-5">
+                            <input type="text" id="inputtext6" class="form-control border-primary"
+                                value="{{ $customerdetail->exename }}" readonly>
+                        </div>
+                        <div class="col-2"></div>
+                        <div class="col-1">
+                            <label for="inputtext6" class="col-form-label">Executive <br> Code</label>
+                        </div>
+                        <div class="col-3">
+                            <input type="text" id="inputtext6" class="form-control border-primary"
+                                value="{{ $customerdetail->exe_id }}" readonly>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row g-3 ">
+                        <div class="col-1">
+                            <label for="inputtext6" class="col-form-label">Mouza</label>
+                        </div>
+                        <div class="col-3">
+                            <input type="text" id="inputtext6" class="form-control border-primary"
+                                value="{{ $customerdetail->mouza }}" readonly>
+                        </div>
+                        <div class="col-1">
+                            <label for="inputtext6" class="col-form-label">Plot no.</label>
+                        </div>
+                        <div class="col-3">
+                            <input type="text" id="inputtext6" class="form-control border-primary"
+                                value="{{ $customerdetail->plotNo }}" readonly>
+                        </div>
+                        <div class="col-1">
+                            <label for="inputtext6" class="col-form-label">Khasara <br> No</label>
+                        </div>
+                        <div class="col-3">
+                            <input type="text" id="inputtext6" class="form-control border-primary"
+                                value="{{ $customerdetail->khasaraNo }}" readonly>
+                        </div>
+                        <div class="col-1">
+                            <label for="inputtext6" class="col-form-label">Plot Cost</label>
+                        </div>
+                        <div class="col-3">
+                            <input type="text" id="inputtext6" class="form-control border-primary"
+                                value="{{ $customerdetail->totalPlotC }}" readonly>
+                        </div>
+                        <div class="col-1">
+                            <label for="inputtext6" class="col-form-label">Size</label>
+                        </div>
+                        <div class="col-3">
+                            <input type="text" id="inputtext6" class="form-control border-primary"
+                                value="{{ $customerdetail->size }}" readonly>
+                        </div>
+                        <div class="col-1">
+                            <label for="inputtext6" class="col-form-label">Type</label>
+                        </div>
+                        <div class="col-3">
+                            <input type="text" id="inputtext6" class="form-control border-primary"
+                                value="{{ $customerdetail->plot_type }}" readonly>
+                        </div>
+                    </div>
+                @endforeach
             </div>
             <div class="p-3">
                 <table class="table table-striped table-border">
@@ -130,6 +143,12 @@
                             <th>Executive details</th>
                         </tr>
                     </thead>
+                    <tbody>
+
+                        <tr>
+                            <th></th>
+                        </tr>
+                    </tbody>
                 </table>
             </div>
         </div>
